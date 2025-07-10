3c7c6be0a0e14131e08b6f1aa6cb9af29995ac0b
Mohd Ahsan <mohdahsanahsan1@gmail.com>
2025-07-10 19:26:50 +0530

    added more pics

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
 
     return (
-        <div className="sm:h-24 h-16 flex justify-between items-center px-7 relative z-20 bg-[rgb(0,0,0,0.4)]">
-            <img src="/icons/logo.png" alt="" className="h-[70%] sm:mx-0 mx-auto brightness-125" />
+        <div className="sm:h-24 h-16 flex justify-between items-center pl-4 pr-7 relative z-20 bg-gradient-to-r from-red-600 to-[rgb(0,0,0,0.4)">
+            <img src="/icons/logo.png" alt="" className="h-[85%] sm:mx-0 brightness-125" />
 
             <div className="sm:block hidden">
                 <ol className="flex gap-x-6 text-white text-xl font-medium">
