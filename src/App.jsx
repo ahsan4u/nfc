60e1cb7b40fa5e0695bc64ab37af55c5ad9ec31f
Mohd Ahsan <mohdahsanahsan1@gmail.com>
2025-08-30 11:00:05 +0530

    added lazy load for fast intraction with pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import { useEffect, useRef, useState } from "react";
+import { lazy, useState, Suspense } from "react";
 import Header from "./components/Header";
-import { useLocation } from "react-router-dom";
-import ComingSoon from "./components/ComingSoon";
 import HeroBanner from "./components/HeroBanner";
+const ComingSoon = lazy(() => import('./components/ComingSoon'));
+const Images = lazy(() => import('./components/Images'));
 
 export default function App() {
   const [open, setOpen] = useState(false);
 
   return (<>
 
-    <ComingSoon open={open} setOpen={setOpen} />
+    <Suspense fallback={null}>
+      <ComingSoon open={open} setOpen={setOpen} />
+    </Suspense>
 
     <Header />
     <div className="absolute top-0 w-full">
-      <HeroBanner setOpen={setOpen}/>
-
-      <div className="mt-6 flex flex-col gap-y-6">
-        <img src="/banner-2.jpg" alt="" className="w-full" />
-        <img src="/banner-4.jpg" alt="" className="w-full" />
-        <img src="/banner-1.jpg" alt="" className="w-full" />
-        <img src="/coming-soon-2.avif" alt="" className="w-full" />
-        <img src="/banner-3.jpg" alt="" className="w-full" />
-      </div>
+      <HeroBanner setOpen={setOpen} />
+    <Suspense fallback={null}>
+    <Images/>
+    </Suspense>
     </div>
   </>)
 }
---
+++ src/components/Images.jsx
@@ -0,0 +1,11 @@
+export default function Images() {
+    return (
+        <div className="mt-6 flex flex-col gap-y-6">
+            <img src="/banner-2.jpg" alt="" className="w-full" />
+            <img src="/banner-4.jpg" alt="" className="w-full" />
+            <img src="/banner-1.jpg" alt="" className="w-full" />
+            <img src="/coming-soon-2.avif" alt="" className="w-full" />
+            <img src="/banner-3.jpg" alt="" className="w-full" />
+        </div>
+    )
+}
