8f507c01e6b1d97aaef1b66ed9afffe62f39b268
Mohd Ahsan <mohdahsanahsan1@gmail.com>
2025-09-08 17:59:00 +0530

    added navigations for different category dishes

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,24 @@ import HeroBanner from "./components/HeroBanner";
 import ProductCard from "./components/ProductCard";
 import Footer from "./components/Footer";
 const ComingSoon = lazy(() => import('./components/ComingSoon'));
-const Images = lazy(() => import('./components/Images'));
 
-export default function App() {
-  const [open, setOpen] = useState(false);
 
-  const dishes = [
+const dishes = [
     { img: 'burger', head: 'Burger', menu: [{ name: 'Chicken Burger', price: 80 }, { name: 'Patty Burger', price: 120 }, { name: 'Zingar Burger', price: 190 }] },
-    { img: 'sandwitch', head: 'Sandwitch', menu: [{ name: 'Veg. Sandwitch', price: 50 }, { name: 'Paneer Sandwitch', price: 60 }, { name: 'Chicken Sandwitch', price: 60 }] },
+    { img: 'sandwitch', head: 'Sandwich', menu: [{ name: 'Veg. Sandwich', price: 50 }, { name: 'Paneer Sandwich', price: 60 }, { name: 'Chicken Sandwich', price: 60 }] },
     { img: 'roll', head: 'Roll', menu: [{ name: 'Egg Roll', price: 50 }, { name: 'Paneer Roll', price: 80 }, { name: 'Chicken Roll', price: 80 }] },
   ]
+export default function App() {
+  const [open, setOpen] = useState(false);
+  const [items, setItems] = useState(dishes);
+  const [active, setActive] = useState('All');
+  
+  function setActiveFn(e) {
+    const {textContent} = e.target;
+    textContent !== 'All'? setItems(dishes.filter(a=>a.head == textContent)) : setItems(dishes);
+    setActive(textContent);
+  }
+  
 
   return (<>
 
@@ -26,10 +34,15 @@ export default function App() {
       <HeroBanner setOpen={setOpen} />
     </div>
 
-    <div className="h-56" />
-    <h2 className="text-center font-bold text-white text-xl pt-4">🔥Pick Your Obsession🔥</h2>
-    {
-      dishes.map(dish => <Fragment key={dish.img}>
+    <div className="h-60" />
+    <h2 className="text-center font-bold text-white text-xl mt-4">🔥Pick Your Obsession🔥</h2>
+    <ol className="flex text-white gap-x-4 mt-4 mx-2 overflow-x-scroll snap-x snap-mandatory">
+      {
+        ['All','Burger', 'Sandwich', 'Roll', 'Pizza', 'Momos', 'Mandi', 'Shakes', 'Mojito'].map(item=><li key={item} ><button onClick={setActiveFn} className={`snap-start px-3 py-0.5 rounded-md bg-[#ffffff0c] border-2 ${active == item? 'border-amber-600 text-amber-500': 'border-[#ffffff19]'}`}>{item}</button></li>)
+      }
+    </ol>
+    { items.length < 1 ? <p className="text-center my-[15vh]"><span className="px-4 py-2 bg-red-700 text-white rounded-md">Currently Unavailable</span></p> :
+      items.map(dish => <Fragment key={dish.img}>
         <h3 className="font-bold text-2xl min-w-1/2 inline-block py-2 pl-3 mt-8 rounded-r-full text-yellow-400 bg-[#1c1c1c]">{dish.head}</h3>
         <div className="mx-3 space-y-4 mt-4">
           {dish.menu.map((item, idx) => <ProductCard key={idx} data={{ img: dish.img, ...item }} setOpen={setOpen} />)}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,6 @@ export default function Header() {
         <div className="sm:h-24 h-16 flex justify-between items-center pl-4 pr-7 relative z-20 bg-gradient-to-r from-red-600 to-[rgb(0,0,0,0.4)">
             <img src="/icons/logo.png" alt="" className="h-[85%] sm:mx-0 brightness-125" />
 
-            <div className="sm:block hidden">
-                <ol className="flex gap-x-6 text-white text-xl font-medium">
-                    <li>Home</li>
-                    <li>Products</li>
-                    <li>Abouts us</li>
-                    <li>Contact Us</li>
-                    <li>Terms & condition</li>
-                </ol>
-            </div>
         </div>
     )
 }
