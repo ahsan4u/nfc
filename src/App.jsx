dc524c0139a182228f6bb3a6aad8ce386872db8b
Mohd Ahsan <mohdahsanahsan1@gmail.com>
2025-09-09 12:56:35 +0530

    added text animation and new font

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ export default function App() {
     </div>
 
     <div className="h-60" />
-    <h2 className="text-center font-bold text-white text-xl mt-4">🔥Pick Your Obsession🔥</h2>
+    <h2 className="text-center font-bold text-white text-xl mt-4 bogart tracking-wider">🔥Pick Your Obsession🔥</h2>
     <ol className="flex text-white gap-x-4 mt-4 mx-2 overflow-x-scroll snap-x snap-mandatory">
       {
         ['All','Roll', 'Burger', 'Sandwich', 'Pizza', 'Momos', 'Mandi', 'Shakes', 'Mojito'].map(item=><li key={item} ><button onClick={setActiveFn} className={`snap-start px-3 py-0.5 rounded-md bg-[#ffffff0c] border-2 ${active == item? 'border-amber-600 text-amber-500': 'border-[#ffffff19]'}`}>{item}</button></li>)
@@ -43,7 +43,7 @@ export default function App() {
     </ol>
     { items.length < 1 ? <p className="text-center my-[15vh]"><span className="px-4 py-2 bg-red-700 text-white rounded-md">Currently Unavailable</span></p> :
       items.map(dish => <Fragment key={dish.img}>
-        <h3 className="font-bold text-2xl min-w-1/2 inline-block py-2 pl-3 mt-8 rounded-r-full text-yellow-400 bg-[#1c1c1c]">{dish.head}</h3>
+        <h3 className="font-bold text-2xl min-w-1/2 inline-block py-2 pl-3 mt-8 rounded-r-full text-amber-500 bg-[#1c1c1c]">{dish.head}</h3>
         <div className="mx-3 space-y-4 mt-4">
           {dish.menu.map((item, idx) => <ProductCard key={idx} data={{ img: dish.img, ...item }} setOpen={setOpen} />)}
         </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
 export default function Footer() {
 
     return (
-        <div className="mt-10 h-52 bg-[#3b2424]">
+        <div className="mt-10 h-52 bg-[#340c0c]">
                 <div className="ml-3 pt-3 flex flex-col items-start justify-between h-full">
-                    <img src="/icons/logo.png" alt="Nawab Food Court"  className="h-16"/>
+                    <div className="flex items-end">
+                        <img src="/icons/logo.png" alt="Nawab Food Court"  className="h-16"/>
+                        <p className="text-slate-500 loader-text">A Place to Remember</p>
+                    </div>
 
                     <div className="mb-8">
-                        <h4 className="sm:text-xl text-lg font-bold ml-1 text-slate-400 w-[70%]">Follow us on</h4>
+                        <h4 className="sm:text-xl text-lg font-bold ml-1 permanent-marker-font text-white tracking-widest">Follow us on</h4>
                         <div className="sm:mt-5 mt-2 flex sm:gap-4 gap-2.5 sm:h-10 h-9  sm:text-lg text-sm">
                             <a href="https://www.facebook.com/nfconline" target="_blank" className="h-full flex items-center justify-center bg-slate-200 aspect-square rounded-full"><i className="fa-brands fa-facebook-f text-blue-700"/></a>
                             <a href="https://www.instagram.com/nawab_food_court/" target="_blank" className="h-full flex items-center justify-center bg-slate-200 aspect-square rounded-full"><i className="fa-brands fa-instagram text-red-500"/></a>
