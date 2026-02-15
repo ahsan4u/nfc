ac11ab8331d5e24eb8c41fa8c606349e4e3c2e47
Mohd Ahsan <mohdahsanahsan1@gmail.com>
2026-02-15 08:48:01 +0530

    update logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
 
     return (
         <div className="sm:h-24 h-16 flex justify-between items-center pl-4 pr-7 relative z-20 bg-gradient-to-r from-red-600 to-[rgb(0,0,0,0.4)">
-            <div className="flex items-end h-[85%]">
+            <div className="flex items-end h-[92x%]">
                 <img src="/icons/logo.png" alt="" className="h-full sm:mx-0" />
                 <p className="text-slate-500 loader-text text-sm tracking-widest dancing-script-font">Taste That Brings You Back</p>
             </div>
