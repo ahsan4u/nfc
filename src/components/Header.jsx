f8afe3f0e5476cf0e881818837b39d7d91cc49e7
Mohd Ahsan <mohdahsanahsan1@gmail.com>
2026-07-11 12:27:02 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 export default function Header() {
-    const phrases = ["ESTD 2026 | LEGACY 1974", "Taste the royal flavour"];
+    const phrases = ["ESTD 2026 | LEGACY 1974", "Cafe & Bakehouse"];
     const [index, setIndex] = useState(0);
     const [subIndex, setSubIndex] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
@@ -35,11 +35,11 @@ export default function Header() {
     return (
         <div className="sm:h-20 h-16 flex justify-between items-center px-4 sm:px-8 fixed top-0 w-full z-40 bg-[#0a0a0a]/60 backdrop-blur-xl border-b border-white/10 shadow-2xl">
             <div className="flex items-center gap-2 h-[80%]">
-                <img src="/icons/logo.png" alt="NFC Logo" className="h-full -mt-0.5 object-contain drop-shadow-[0_0_15px_rgba(255,255,255,0.05)]" />
+                <img src="/icons/new-logo.png" alt="NFC Logo" className="h-full -mt-0.5 object-contain drop-shadow-[0_0_15px_rgba(255,255,255,0.05)]" />
                 <div className="flex flex-col justify-center gap-1.5 -mb-1">
-                    <h1 className="text-white text-base sm:text-xl font-black tracking-[0.25em] leading-tight uppercase bogart">NAWAB FUSION CAFE</h1>
+                    <h1 className="text-white text-base sm:text-xl font-black tracking-[0.25em] leading-tight uppercase bogart">NAWAB SAHAB</h1>
                     <p className="text-amber-500/90 text-[8px] sm:text-[10px] tracking-[0.4em] font-bold uppercase opacity-80 shimmer-text min-h-[1em] flex items-center">
-                        Taste the royal flavour
+                        Cafe & Bakehouse
                         {/* {phrases[index].substring(0, subIndex)} */}
                         {/* <span className="w-[2px] h-[1em] bg-amber-500 ml-1 animate-cursor inline-block"></span> */}
 
